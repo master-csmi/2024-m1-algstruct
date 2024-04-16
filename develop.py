949e9db45611b908135930fdd2702d4c24433943
Blessed-joseph <komi-joseph-beni.assigbe@etu.unistra.fr>
2024-04-16 10:36:13 +0200

    development of the code

--- develop.py
+++ develop.py
@@ -1,4 +1,4 @@
-from _future_ import print_function
+from __future__ import print_function
 import argparse
 import torch
 import torch.nn as nn
@@ -13,72 +13,94 @@ import matplotlib.pyplot as plt
 
 
 class Morphism(nn.Module):
-    def _init_(self, name = 'Morphisme R^n --> E', dim_E = 1, neurons = 6):
+    def __init__ (self, name = 'Morphisme R^n --> E', dim_E = 1, neurons = 6):
         print(f'[Model] name : {name}')
         print(f'[Model] dim E : {dim_E}')
         print(f'[Model] no. neurons per layers : {neurons}')
-        super(Morphism, self)._init_()
+        super(Morphism, self).__init__()
         # layers for plus : E --> E
         self.fc1 = nn.Linear(dim_E, neurons)
         self.fc2 = nn.Linear(neurons, neurons)
-        self.fc3 = nn.Linear(neurons, dim_E)
+        self.fc3 = nn.Linear(neurons, neurons)
+        self.fc4 = nn.Linear(neurons, dim_E)
     def forward(self, x):
         x = self.fc1(x)
         x = self.fc2(x)
-        output = self.fc3(x)
+        x = self.fc3(x)
+        output = self.fc4(x)
         return output
 # f inv theta
 class InverseMorphism(nn.Module):
-    def _init_(self, name = 'Inverse E --> R^n', dim_E = 1, neurons = 6):
+    def __init__ (self, name = 'Inverse E --> R^n', dim_E = 1, neurons = 6):
+        
         print(f'[Model] name : {name}')
         print(f'[Model] dim E : {dim_E}')
         print(f'[Model] no. neurons per layers : {neurons}')
-        super(InverseMorphism, self)._init_()
+        super(InverseMorphism, self).__init__()
         # layers for plus : E --> E
         self.fc1 = nn.Linear(dim_E, neurons)
         self.fc2 = nn.Linear(neurons, neurons)
-        self.fc3 = nn.Linear(neurons, dim_E)
+        self.fc3= nn.Linear(neurons, neurons)
+        
+        self.fc4 = nn.Linear(neurons, dim_E)
     def forward(self, x):
         x = self.fc1(x)
         x = self.fc2(x)
-        output = self.fc3(x)
+        x = self.fc3(x)
+        output = self.fc4(x)
         return output
+    
+    
 class LoiBinaire(nn.Module):
-    def _init_(self, name = 'Loi binaire ExE-->E', dim_E = 1, neurons = 6):
+    def __init__ (self, name = 'Loi binaire ExE-->E', dim_E = 1, neurons = 6):
+        
         print(f'[Model] name : {name}')
         print(f'[Model] dim E : {dim_E}')
         print(f'[Model] no. neurons per layers : {neurons}')
-        super(LoiBinaire, self)._init_()
+        super(LoiBinaire, self).__init__()
         # layers for plus : ExE --> E
         self.fc1 = nn.Linear(2 * dim_E, neurons)
         self.fc2 = nn.Linear(neurons, neurons)
-        self.fc3 = nn.Linear(neurons, dim_E)
+        self.fc3 = nn.Linear(neurons, neurons)
+        self.fc4 = nn.Linear(neurons, dim_E)
     def forward(self, x, y):
         z = torch.cat([x,y], axis=1) # [K,d], [K,d] ---> [K, 2*d]
         z = self.fc1(z)
         z = self.fc2(z)
-        output = self.fc3(z)
+        z = self.fc3(z)
+        output = self.fc4(z)
         return output
 # scalaire product of structure
+
 class LoiScalaire(nn.Module):
-    def _init_(self, name = 'Loi Scalaire RxE-->E', dim_E = 1, neurons = 6):
+    def __init__ (self, name = 'Loi Scalaire RxE-->E', dim_E = 1, neurons = 6):
+        
         print(f'[Model] name : {name}')
         print(f'[Model] dim E : {dim_E}')
         print(f'[Model] no. neurons per layers : {neurons}')
-        super(LoiScalaire, self)._init_()
-        # layers for plus : ExE --> E
+        super(LoiScalaire, self).__init__()
+        # layers for plus : KxE --> E
+        
+
         self.fc1 = nn.Linear(dim_E, neurons)
         self.fc2 = nn.Linear(neurons, neurons)
-        self.fc3 = nn.Linear(neurons, dim_E)
+        self.fc3 = nn.Linear(neurons, neurons)
+        self.fc4 = nn.Linear(neurons, dim_E)
+        
+        # alpha est un  scalaire,  dim_E est la dimension de l'espace E
+        
     def forward(self, alpha, x):
-        z = torch.cat([x], axis=1) # [K,d], [K,d] ---> [K, 2*d]
+        z = alpha * x # [K,1], [K,d] ---> [K, d]
         z = self.fc1(z)
         z = self.fc2(z)
-        output = self.fc3(z)
+        z = self.fc3(z)
+        output = self.fc4(z)
         return output
+    
+    
 class Group(nn.Module):
-    def _init_(self, K,  dim_E = 2 , neurons = 6, name = 'Groupe (E,+)'):
-        super(Group, self)._init_()
+    def __init__ (self, K,  dim_E = 1 , neurons = 6 , name = 'Groupe (E,+)'):
+        super(Group, self).__init__()
         self.f    = Morphism(dim_E = dim_E, neurons = neurons)
         self.fi   = InverseMorphism(dim_E = dim_E, neurons = neurons)
         self.plus = LoiBinaire(dim_E = dim_E, neurons = neurons)
@@ -86,51 +108,41 @@ class Group(nn.Module):
         # losses
         self.loss_1 = lambda x, y : torch.linalg.vector_norm(self.plus(x , y) - self.f( self.fi(x) + self.fi(y)) )**2
         self.loss_2 = lambda alpha, x : torch.linalg.vector_norm(self.scalaire(alpha , x) - self.f( alpha*self.fi(x)) )**2
-        self.loss_3 = lambda x, y : torch.linalg.vector_norm(x - y)  + torch.linalg.vector_norm(self.fi( self.f(x))- self.fi( self.f(y) ))
-        # # losses
-        #self.loss_zero     = lambda x    : torch.linalg.vector_norm(self.plus(self.plus(self.zero * torch.ones(K, dim_E), self.zero * torch.ones(K, dim_E)),self.op(self.zero * torch.ones(K, dim_E))))
-        #self.loss_neutral  = lambda x    : torch.linalg.vector_norm(self.plus(self.plus(x, self.zero * torch.ones(K, dim_E)), self.op(x)))                                    # x + 0 = x
-        #self.loss_assoc = lambda x, y, z : torch.linalg.vector_norm(self.plus(self.plus(x, self.plus(y, z)), self.op(self.plus(self.plus(x, y), z))))           # x+(y+z) = (x+y)+z
-        #self.loss_comm  = lambda x, y    : torch.linalg.vector_norm(self.plus(self.plus(self.plus(x, y), self.op(self.plus(y,x))),self.zero * torch.ones(K, dim_E)))          # x+y = y+x
-        #self.loss_op    = lambda x       : torch.linalg.vector_norm(self.plus(self.plus(x, self.op(x)), self.zero * torch.ones(K, dim_E) ))                                    # x + (-x) = 0
-        # Total loss (can be weighted)
-        self.loss  = lambda x,y:  self.loss_1(x,y)
-        self.loss  = lambda x,alpha:  self.loss_2(alpha,x)
-        self.loss  = lambda x,y:  self.loss_3(x,y)
-        self.loss  = lambda x,y,alpha:  self.loss_1(x,y) + self.loss_2(alpha,x) + self.loss_3(x,y)
+
+        #  Total loss can be weighted 
+        self.loss = lambda x, y, alpha : self.loss_1(x, y) + self.loss_2(alpha, x)
+        
     def train(self, X, Y,alpha, optimizer, epoch):
         self.f.train()
         self.fi.train()
         self.plus.train()
         self.scalaire.train()
-        #plt.ion()
-        #figure = plt.figure()
-        #ax = figure.add_subplot(111)
-        #ax.plot(X[:,0], X[:,1], 'X', label='X')
-        #ax.plot(self.zero.data, 'r')
-        #ax[0,1].plot(Y[:,0], Y[:,1], 'Y', label='Y')
-        #ax[0,2].plot(Z[:,0], Z[:,1], 'Z', label='Z')
+
         for i in range(epoch):
             optimizer.zero_grad()
             L1 = self.loss_1(X, Y)
             L2 = self.loss_2(alpha, X)
-            L3 = self.loss_3(X, Y)
-            loss = L1 + L2 + L3
-            #if i % 10 == 0:
-            print('Epoch {}/{} -\t Loss 1: {:.6f}\t Loss 2: {:.6f}\t Loss 3: {:.6f}\t Total Loss: {:.6f}'
-                      .format(i, epoch, L1.item(), L2.item(),L3.item(), loss.item()))
+
+            loss = L1 + L2 
+            print('Epoch {}/{} -\t Loss 1: {:.6f}\t Loss 2: {:.6f}\t Total Loss: {:.6f}'.format(i, epoch, L1.item(), L2.item(), loss.item()))
+            
             loss.backward(retain_graph=True)
             optimizer.step()
-    def test(self, test_loader):
+            
+            
+            
+def test(self, test_loader):
         pass
+
 # Dataset generation
-def elliptic_curve(K, a=2, b=3):
+def line(K): #a=2, b=3):
+    
     X = 0.3*torch.randn(K, 2).requires_grad_(False)
     Y = 0.3*torch.randn(K, 2).requires_grad_(False)
-    alpha = 0.3*torch.randn(K, 1).requires_grad_(False)
-    X[:,1] = X[:,0] + torch.sin(X[:,0]) # **3 + a*X[:,0] + b
-    Y[:,1] = Y[:,0] + torch.sin(Y[:,0]) #**3 + a*Y[:,0] + b
-    alpha = alpha #**3 + a*alpha + b
+    alpha = torch.randn(K, 1).requires_grad_(False)
+    epislon = 0.1
+    X[:,1] = X[:,0] + epislon * torch.sin(X[:,0]) # **3 + a*X[:,0] + b
+    Y[:,1] = Y[:,0] + epislon * torch.sin(Y[:,0]) #**3 + a*Y[:,0] + b
     return X, Y, alpha
 #def dataset_parabola(K, a=0.5, b=0.5):
     #X = 0.3*torch.randn(K, 2).requires_grad_(False)
@@ -172,23 +184,31 @@ elif use_mps:
     device = torch.device("mps")
 else:
     device = torch.device("cpu")
+    
+    
 # Training datasets
 dim = 2
-K = 200
+K = 1000
 X = tc.rand(K, 2)
 Y = tc.rand(K, 2)
 alpha = tc.rand(K, 1)
 #X, Y, alpha = elliptic_curve(K)
+X, Y, alpha = line(K)
 beta = 1.0  # replace with your value of beta
 #f = tc.vmap(lambda X : X[0]**2)
+
 f = tc.vmap(lambda X : (1/beta) * tc.exp(X[0]))
 fX = f(X)
 fY = f(Y)
+
+# on initalise le Groupe 
 G = Group(K, dim_E = dim, neurons = 32)
-optimizer = optim.Adadelta(list(G.parameters()), lr=args.lr)
-G.train(X, Y,alpha, optimizer, args.epochs)
+optimizer = optim.Adadelta(list(G.parameters()), lr=0.1)
+
+G.train(X, Y, alpha, optimizer, args.epochs)
 plt.plot(X[:, 0], X[:, 1], 'x', label='train X')
 plt.plot(Y[:, 0], Y[:, 1], 'o', label='train Y')
+
 Xe = G.f(X)
 if args.save_model:
     print('Saving model...')
